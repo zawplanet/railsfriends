c7e6a022b93bb81ccec762f128d5bce37c6f65f7
Zaw Lay Pyay Thar <ms.zawlaypyaythar@gmail.com>
2022-04-23 19:07:08 +0630

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  resources :friends
+  # get 'home/index'
+  # root 'home#index'
+  root 'friends#index'
+  get 'home/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
